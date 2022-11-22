fc866b6d82c74dfaa090844f3f0d10ee14954ef1
Gabriele-Granvillano <114691186+Gabriele-Granvillano@users.noreply.github.com>
2022-11-22 19:23:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main()
+{
+    int n = 0, i = 0, primo_num = 1, secondo_num = 0, terzo_num = 0;
+    cin >> n;
+    if (n>=2)
+    {
+        for (i; i < n; i++)
+        {
+            terzo_num = primo_num + secondo_num;
+            primo_num = secondo_num;
+            secondo_num = terzo_num;
+            cout << terzo_num << endl;
+        }
+    } else
+    {
+        cout << "errore" << endl;
+    }
+    return 0;
 }
